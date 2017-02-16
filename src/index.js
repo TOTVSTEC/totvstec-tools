879f433e817cf35ad34938a6879ac97d459b8bf8
Rogerio Ribeiro da Cruz <rogeriorc@gmail.com>
2017-02-16 11:36:13 -0200

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-#!/usr/bin/env node
 'use strict';
 
-process.title = 'cloudbridge';
-
-let argv = require('minimist')(process.argv.slice(2));
-
-if (argv._.length > 1) {
-	if ((argv._[0] === 'android') && (argv._[1] === 'aar')) {
-		let generator = require('./generate_aar');
-
-		generator(argv);
-	}
-}
-//console.log(JSON.stringify(argv, null, 2));
+module.exports = {
+	git: require('./util/git'),
+	npm: require('./util/npm'),
+	version: require('./util/version')
+};
